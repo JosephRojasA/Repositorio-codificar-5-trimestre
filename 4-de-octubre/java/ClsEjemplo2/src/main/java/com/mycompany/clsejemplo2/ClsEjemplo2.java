4fbdf106f356c219adcce8c04192aa9c3ca7767d
jods <jos@outlook.co>
2024-10-04 07:15:35 -0500

    agregar java v1

---
+++ 4-de-octubre/java/ClsEjemplo2/src/main/java/com/mycompany/clsejemplo2/ClsEjemplo2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.clsejemplo2;
+
+/**
+ *
+ * @author APRENDIZ
+ */
+public class ClsEjemplo2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
