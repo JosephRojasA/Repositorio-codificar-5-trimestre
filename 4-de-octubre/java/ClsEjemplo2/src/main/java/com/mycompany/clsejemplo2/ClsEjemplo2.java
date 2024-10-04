2e9939ec5cbe3b6bb60e2ee258388773d0baa0f3
jods <jos@outlook.co>
2024-10-04 07:24:48 -0500

    agregando variables a java documento

--- 4-de-octubre/java/ClsEjemplo2/src/main/java/com/mycompany/clsejemplo2/ClsEjemplo2.java
+++ 4-de-octubre/java/ClsEjemplo2/src/main/java/com/mycompany/clsejemplo2/ClsEjemplo2.java
@@ -4,13 +4,21 @@
 
 package com.mycompany.clsejemplo2;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author APRENDIZ
  */
 public class ClsEjemplo2 {
 
+    
+    
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        for(int est= 0; est <= 2; est++){
+            String nombre =JOptionPane.showInputDialog(null,"ingrese el nombre del estudiante");
+            Float nota1 = Float.parseFloat(JOptionPane.showInputDialog(null,"ingrese la nota del estudiante"));
+            Float nota2 = Float.parseFloat(JOptionPane.showInputDialog(null,"ingrese la nota 2 del estudiante"));
+        }
     }
 }
