4bae55149988c3cbfe4ed5782edf23a0c9db3c91
jods <jos@outlook.co>
2024-10-08 09:13:57 -0500

    8 de octubre

--- 4-de-octubre/java/ClsEjemplo2Obj/src/main/java/com/mycompany/clsejemplo2obj/ClsEjemplo2Obj.java
+++ 4-de-octubre/java/ClsEjemplo2Obj/src/main/java/com/mycompany/clsejemplo2obj/ClsEjemplo2Obj.java
@@ -14,7 +14,22 @@ import javax.swing.JOptionPane;
 public class ClsEjemplo2Obj {
 
     public static void main(String[] args) {
-        Estudiante estudiante = new Estudiante("pepe");
+        
+        for (int i=1; i<=3; i++)
+        {ss
+            
+            String nombre=JOptionPane.showInputDialog(null, "ingrese el nombre del estudiante");
+            float nota1=Float.parseFloat(JOptionPane.showInputDialog(null,"ingrese la nota 1 del estudiante"));
+            float nota2=Float.parseFloat(JOptionPane.showInputDialog(null,"ingrese la nota 2 del estudiante"));
+            est=new Estudiante(nombre);
+            est.setNotaParcial1(nota1);
+            est.setNotaParcial1(nota2);
+            est.calcularNotaFinal();
+            JOptionPane.showMessageDialog(null, "el estudiante"+ ext.nombre+"\nObtuvo una nota final de "+est.calculo);
+            
+            
+        }
+        /*Estudiante estudiante = new Estudiante("pepe");
         estudiante.setNotaParcial1((float)1.5); // Usando 'f' para indicar que es un float
         estudiante.setNotaParcial2((float)4.0);
         estudiante.calcularNotaFinal();     
@@ -30,6 +45,6 @@ public class ClsEjemplo2Obj {
         
         JOptionPane.showMessageDialog(null, "El estudiante " + est2.getNombre() + 
             " obtuvo la nota final: " + est2.obtenerNotaFinal() + 
-            ". Por consiguiente " + est2.obtenerMensaje());
+            ". Por consiguiente " + est2.obtenerMensaje()); */
     }
 }
